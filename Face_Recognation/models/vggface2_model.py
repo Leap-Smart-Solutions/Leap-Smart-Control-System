8e1f8cbf07c44ceb386da2aac14fb1093386299c
AyaNabil <ayanabil297@gmail.com>
2025-04-10 17:20:27 +0200

    Load VGGFace2 Model & Generate Embeddings

--- Face_Recognation/models/vggface2_model.py
+++ Face_Recognation/models/vggface2_model.py
@@ -1,15 +1,15 @@
-# models/vggface2_model.py
 from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
 from keras.models import Model
 import numpy as np
 
 def load_vggface2_model():
-    base_model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
-    return base_model
+    model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+    return model
 
 def get_embedding(model, face_pixels):
     face_pixels = face_pixels.astype('float32')
-    face_pixels = preprocess_input(face_pixels, version=2)
+    face_pixels = preprocess_input(face_pixels, version=2)  # For ResNet
     sample = np.expand_dims(face_pixels, axis=0)
-    return model.predict(sample)[0]
+    yhat = model.predict(sample)
+    return yhat[0]
