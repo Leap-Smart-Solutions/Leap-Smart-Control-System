2544d36c62e99188075fc59f7517bd8c24ef28f1
AyaNabil <ayanabil297@gmail.com>
2025-04-10 23:30:26 +0200

    update file path

--- Face_Recognition/main.py
+++ Face_Recognition/main.py
@@ -1,9 +1,27 @@
+import os
 from face_embedding import generate_embeddings
-from recognizer import recognize_face
+from utils.db_manager import initialize_db
 
-# Step 1: Generate embeddings
-generate_embeddings(r"dataset", r"embeddings\embeddings.pkl")
+# Define paths
+dataset_path = "dataset"
+embedding_path = "embeddings/face_data.pkl"
+db_path = "database/embeddings.db"
+
+# Ensure both folders exist
+os.makedirs(os.path.dirname(embedding_path), exist_ok=True)
+os.makedirs(os.path.dirname(db_path), exist_ok=True)
+
+# Initialize database
+initialize_db(db_path)
+
+# Generate embeddings and store them
+generate_embeddings(dataset_path, embedding_path)
+
 
 # Step 2: Recognize new face
-result = recognize_face(r"test_image.jpeg", r"embeddings\embeddings.pkl")
+result = recognize_face("test_image.jpg", "embeddings/face_data.pkl")
 print("Match:", result)
+
+print(f"[✔] Embeddings stored in database: {db_path}")
+print(f"[✔] Embeddings stored in pickle: {embedding_path}")
+print("[✔] Face recognition system is ready to use.")
