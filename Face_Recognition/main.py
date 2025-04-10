2978a5903926305f1d4318d1c6ee9130b8853a1d
AyaNabil <ayanabil297@gmail.com>
2025-04-10 21:01:20 +0200

    Update main

--- Face_Recognition/main.py
+++ Face_Recognition/main.py
@@ -5,5 +5,5 @@ from recognizer import recognize_face
 generate_embeddings(r"dataset", r"embeddings\embeddings.pkl")
 
 # Step 2: Recognize new face
-result = recognize_face(r"test_image.jpg", r"embeddings\embeddings.pkl")
+result = recognize_face(r"test_image.jpeg", r"embeddings\embeddings.pkl")
 print("Match:", result)
