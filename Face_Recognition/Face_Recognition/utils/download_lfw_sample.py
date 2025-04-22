1daa04128635aeae3030a9e82f2597b9e5475cd5
AyaNabil <ayanabil297@gmail.com>
2025-04-23 01:26:31 +0200

    Required

---
+++ Face_Recognition/Face_Recognition/utils/download_lfw_sample.py
@@ -0,0 +1,21 @@
+import os
+from sklearn.datasets import fetch_lfw_people
+import cv2
+
+def download_lfw_subset(save_path='Face_Recognition/dataset', people_limit=3, images_per_person=3):
+    os.makedirs(save_path, exist_ok=True)
+    dataset = fetch_lfw_people(min_faces_per_person=images_per_person, resize=1.0, color=True)
+
+    for i in range(len(dataset.images)):
+        person = dataset.target_names[dataset.target[i]]
+        person_dir = os.path.join(save_path, person.replace(' ', '_'))
+        os.makedirs(person_dir, exist_ok=True)
+        img = dataset.images[i]
+        img_bgr = cv2.cvtColor(img.astype('uint8'), cv2.COLOR_RGB2BGR)
+        filename = os.path.join(person_dir, f'{i}.jpg')
+        cv2.imwrite(filename, img_bgr)
+
+    print(f"Saved a subset of LFW to: {save_path}")
+
+if __name__ == "__main__":
+    download_lfw_subset()
