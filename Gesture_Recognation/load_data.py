7bfc5574fd01ed31f808f9969f93e2c6824cb604
Muhannad Khaled <119460687+Muhannad-Khaled@users.noreply.github.com>
2025-06-04 18:05:12 +0300

    Delete Gesture_Recognation directory

--- Gesture_Recognation/load_data.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def load_data_from_csv(filepath):
-    return pd.read_csv(filepath)
--- Gesture_Recognation/test_stream.py
+++
@@ -1,5 +0,0 @@
-# test_stream.py
-from imu.stream_imu import stream_imu_data
-
-for line in stream_imu_data(port='COM3'):  # Update the port for your system
-    print(line)
